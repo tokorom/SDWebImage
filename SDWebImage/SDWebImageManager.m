9ff13d568a7d47d4903014d6f0e121d284d87bf7
tokorom <tokorom@gmail.com>
2016-06-24 12:29:10 +0900

    Support SDWebImageIgnoreGIFAnimation

--- SDWebImage/SDWebImageManager.m
+++ SDWebImage/SDWebImageManager.m
@@ -151,7 +151,7 @@
     }
     NSString *key = [self cacheKeyForURL:url];
 
-    operation.cacheOperation = [self.imageCache queryDiskCacheForKey:key done:^(UIImage *image, SDImageCacheType cacheType) {
+    operation.cacheOperation = [self.imageCache queryDiskCacheForKey:key options:options done:^(UIImage *image, SDImageCacheType cacheType) {
         if (operation.isCancelled) {
             @synchronized (self.runningOperations) {
                 [self.runningOperations removeObject:operation];
